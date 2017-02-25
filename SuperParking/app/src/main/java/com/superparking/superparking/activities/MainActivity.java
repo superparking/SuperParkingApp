1a9352df14886405d25b08104c0ce1a5fff090d3
Miguel Grado <mgradob@gmail.com>
2017-02-25 09:57:19 -0700

    initial commit

---
+++ SuperParking/app/src/main/java/com/superparking/superparking/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.superparking.superparking.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.superparking.superparking.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
